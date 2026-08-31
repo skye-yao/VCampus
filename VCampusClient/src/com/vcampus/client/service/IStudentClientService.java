b9da6fbcb5fa3a535d2a330e8861e9f7bce22cba
XavrN <xavirnotfound@outlook.com>
2026-08-31 14:06:53 +0800

    Delete VCampusClient/src/com/vcampus/client directory

--- VCampusClient/src/com/vcampus/client/service/IStudentClientService.java
+++
@@ -1,8 +0,0 @@
-package com.vcampus.client.service;
-import java.util.function.Consumer;
-import com.vcampus.common.entity.*;import com.vcampus.common.protocol.Message;import com.vcampus.common.vo.StudentReviewVO;
-public interface IStudentClientService {
- void queryOverview(Consumer<Message> callback); void submitChangeRequest(StudentChangeRequest request,Consumer<Message> callback); void queryMyRequests(Consumer<Message> callback); void cancelChangeRequest(long requestId,Consumer<Message> callback);
- void listStudents(Consumer<Message> callback); void queryStudentOverview(String studentId,Consumer<Message> callback); void listPendingRequests(Consumer<Message> callback); void queryChangeRequest(long requestId,Consumer<Message> callback); void reviewChangeRequest(StudentReviewVO review,Consumer<Message> callback);
- void updateStudentByAdmin(Student student,Consumer<Message> callback); void addAward(StudentAward award,Consumer<Message> callback); void updateAward(StudentAward award,Consumer<Message> callback); void deleteAward(long id,Consumer<Message> callback); void addAid(StudentAid aid,Consumer<Message> callback); void updateAid(StudentAid aid,Consumer<Message> callback); void deleteAid(long id,Consumer<Message> callback);
-}
