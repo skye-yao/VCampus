f3df7ed9ecf65debf8ce7c2c171c6054dbddfb7f
XavrN <xavirnotfound@outlook.com>
2026-08-31 14:07:25 +0800

    Delete VCampusServer/src/com/vcampus/server directory

--- VCampusServer/src/com/vcampus/server/dao/StudentChangeItemDAO.java
+++
@@ -1,6 +0,0 @@
-package com.vcampus.server.dao;
-import java.sql.*;import java.util.*;import com.vcampus.common.entity.StudentChangeItem;
-public class StudentChangeItemDAO {
-    public void insertAll(Connection c,long requestId,List<StudentChangeItem> items)throws SQLException{try(PreparedStatement ps=c.prepareStatement("INSERT INTO tblStudentChangeItem(requestId,fieldName,oldValue,newValue) VALUES(?,?,?,?)")){for(StudentChangeItem i:items){ps.setLong(1,requestId);ps.setString(2,i.getFieldName());ps.setString(3,i.getOldValue());ps.setString(4,i.getNewValue());ps.addBatch();}ps.executeBatch();}}
-    public List<StudentChangeItem> findByRequestId(Connection c,long id)throws SQLException{List<StudentChangeItem> out=new ArrayList<>();try(PreparedStatement ps=c.prepareStatement("SELECT * FROM tblStudentChangeItem WHERE requestId=? ORDER BY itemId")){ps.setLong(1,id);try(ResultSet rs=ps.executeQuery()){while(rs.next()){StudentChangeItem i=new StudentChangeItem();i.setItemId(rs.getLong("itemId"));i.setRequestId(id);i.setFieldName(rs.getString("fieldName"));i.setOldValue(rs.getString("oldValue"));i.setNewValue(rs.getString("newValue"));out.add(i);}}}return out;}
-}
--- VCampusServer/src/com/vcampus/server/handler/UserHandler.java
+++
@@ -1,8 +0,0 @@
-package com.vcampus.server.handler;
-import com.vcampus.common.protocol.*;
-public class UserHandler {
-    public Message handle(Message request) {
-        Message response=new Message(MessageType.RESPONSE,"user",request.getAction(),MessageCode.ERROR);
-        response.setMessage("用户模块尚未实现"); return response;
-    }
-}
