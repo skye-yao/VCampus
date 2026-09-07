e35ca50fb8a8a26d5d214b4dd9a3ae058936e76f
skye-yao <1731218593@qq.com>
2026-09-07 18:14:16 +0800

    不用管这个

--- VCampusCommon/src/enums/StudentAidStatus.java
+++
@@ -1,5 +0,0 @@
-package enums;
-public enum StudentAidStatus {
-    //学生资助状态：待发放、已发放、已取消
-    PENDING, ISSUED, CANCELLED
-}
--- VCampusCommon/src/enums/StudentAwardType.java
+++
@@ -1,5 +0,0 @@
-package enums;
-public enum StudentAwardType {
-    //奖学金、荣誉称号、竞赛奖励、实践奖励、科研奖励、其他
-    SCHOLARSHIP, HONOR, COMPETITION, RESEARCH, PRACTICE, OTHER
-}
